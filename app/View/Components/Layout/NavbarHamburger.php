e068dfc820d1757b90e9b41f32e97e31bc1801f4
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-07-21 22:17:10 +0200

    réécriture paths classes php crées par laravel

--- app/View/Components/Layout/NavbarHamburger.php
+++ app/View/Components/Layout/NavbarHamburger.php
@@ -23,6 +23,6 @@ class NavbarHamburger extends Component
      */
     public function render()
     {
-        return view('components.layout.navbar-hamburger');
+        return view('layout.navbar-hamburger');
     }
 }
