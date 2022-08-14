40f1ba58f1cee62d147b77ff7176736da1348c75
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-08-14 07:47:27 +0200

    ajout sous titre coloré, ajout image top, reelief avec cercles derriere images ok alpine ok

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,16 +1,17 @@
 <!-- ====== Hero Section Start -->
-<div class="relative pt-[60px] lg:pt-[80px] pb-[110px] bg-white">
+<div class="relative pt-[60px] lg:pt-[80px] pb-[110px] bg-white dark:bg-slate-800">
     <div class="container">
         <div class="flex flex-wrap -mx-4">
             <div class="w-full lg:w-5/12 px-4">
                 <div class="hero-content">
                     <h1
-                        class="text-dark font-bold text-4xl sm:text-[32px] lg:text-[30px] xl:text-[32px] leading-snug mb-3"
+                        class="text-dark dark:text-gray-200 font-bold text-4xl sm:text-[42px] lg:text-[40px] xl:text-[42px] leading-snug mb-6"
 >
                         <br/>
                         <br/>
+                        <br/>
 Mourad BOUSSIOUF <br/>
-
+                        <span class="px-2 rounded-md text-amber-600">Web_dev</span>
 
                     </h1>
                     <p class="text-base mb-8 text-body-color max-w-[480px]">
@@ -21,7 +22,7 @@ depuis 2 ans, et étudiant à La plateforme, ce site présente mon parcours ains
                     <ul class="flex flex-wrap items-center">
                         <li>
                             <a
-                                href="javascript:void(0)"
+                                href="#projects"
                                 class="
                                   py-4
                                   px-6
@@ -37,76 +38,22 @@ depuis 2 ans, et étudiant à La plateforme, ce site présente mon parcours ains
                                   font-normal
                                   rounded-lg
                                 "
->
-ME CONTACTER
-</a>
-                        </li>
-                        <li>
-                            <a
-                                href="javascript:void(0)"
-                                class="
-                                  py-4
-                                  px-6
-                                  sm:px-10
-                                  lg:px-8
-                                  xl:px-10
-                                  inline-flex
-                                  items-center
-                                  justify-center
-                                  text-center text-base
-                                  font-normal
-                                  text-body-color
-                                  hover:text-primary
-                                "
->
-                                <span class="mr-2">
-                                  <svg
-                                      width="22"
-                                      height="22"
-                                      viewBox="0 0 22 22"
-                                      fill="none"
-                                      xmlns="http://www.w3.org/2000/svg"
-                                          >
-                                    <circle cx="11" cy="11" r="11" fill="#3056D3"/>
-                                    <rect
-                                        x="6.90906"
-                                        y="13.3636"
-                                        width="8.18182"
-                                        height="1.63636"
-                                        fill="white"
-                                            />
-                                    <rect
-                                        x="10.1818"
-                                        y="6"
-                                        width="1.63636"
-                                        height="4.09091"
-                                        fill="white"
-                                            />
-                                    <path
-                                        d="M11 12.5454L13.8343 9.47726H8.16576L11 12.5454Z"
-                                        fill="white"
-                                            />
-                                  </svg>
-                                </span>
-Mes projets terminés
-</a>
-                        </li>
+                            >
+                                Mes projets
+
+                            </a>
+                    </li>
                     </ul>
                     <div class="clients pt-16">
                         <h6
-                            class="
-                    font-normal
-                    text-xs
-                    flex
-                    items-center
-                    text-body-color
-                    mb-2
-                  "
+                            class="font-normal text-xs flex items-center text-body-color mb-2"
 >
 Mon linkedin
 <span
                                 class="w-8 h-[1px] bg-body-color inline-block ml-2"
-></span>
+>
+
+                            </span>
                         </h6>
                         <div class="flex items-center">
                         </div>
@@ -116,6 +63,48 @@ Mon linkedin
             <div class="hidden lg:block lg:w-1/12 px-4"></div>
             <div class="w-full lg:w-6/12 px-4">
                 <div class="lg:text-right lg:ml-auto">
+                    <div class="relative inline-block z-10 pt-11 lg:pt-0">
+                        <img
+                            src="https://cdn.tailgrids.com/1.0/assets/images/hero/hero-image-01.png"
+                            alt="hero"
+                            class="max-w-full lg:ml-auto"
+                        />
+                        <span class="absolute -left-8 -bottom-8 z-[-1]">
+                          <svg
+                              width="93"
+                              height="93"
+                              viewBox="0 0 93 93"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <circle cx="2.5" cy="2.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="2.5" cy="24.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="2.5" cy="46.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="2.5" cy="68.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="2.5" cy="90.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="24.5" cy="2.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="24.5" cy="24.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="24.5" cy="46.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="24.5" cy="68.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="24.5" cy="90.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="46.5" cy="2.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="46.5" cy="24.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="46.5" cy="46.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="46.5" cy="68.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="46.5" cy="90.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="68.5" cy="2.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="68.5" cy="24.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="68.5" cy="46.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="68.5" cy="68.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="68.5" cy="90.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="90.5" cy="2.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="90.5" cy="24.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="90.5" cy="46.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="90.5" cy="68.5" r="2.5" fill="#3056D3"/>
+                            <circle cx="90.5" cy="90.5" r="2.5" fill="#3056D3"/>
+                          </svg>
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
