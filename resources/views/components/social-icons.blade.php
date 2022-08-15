aeb93a16b84f60f3426a87c5cc1f5bedea682352
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-08-15 21:25:06 +0200

    proto footer ko element compressé , edition app.css tailwind

--- resources/views/components/social-icons.blade.php
+++ resources/views/components/social-icons.blade.php
@@ -1,21 +1,25 @@
 <div class="flex items-center mb-6">
     <a
-        href="javascript:void(0)"
+        href="https://www.facebook.com/mourad"
+        target="_blank"
         class="
-                     w-8
-                     h-8
-                     flex
-                     items-center
-                     justify-center
-                     rounded-full
-                     border border-[#E5E5E5]
-                     text-dark
-                     hover:text-white hover:bg-primary hover:border-primary
-                     mr-3
-                     sm:mr-4
-                     lg:mr-3
-                     xl:mr-4
-                     "
+         w-10
+         h-10
+         flex
+         items-center
+         justify-center
+         rounded-full
+         border border-[#E5E5E5]
+         text-dark
+         dark:text-white
+         hover:text-white
+         hover:bg-primary
+         hover:border-primary
+         mr-3
+         sm:mr-4
+         lg:mr-3
+         xl:mr-4
+    "
     >
         <svg
             width="8"
@@ -29,22 +33,26 @@
         </svg>
     </a>
     <a
-        href="javascript:void(0)"
+        href="https://twitter.com/blockchaintriso"
+        target="_blank"
         class="
-                     w-8
-                     h-8
-                     flex
-                     items-center
-                     justify-center
-                     rounded-full
-                     border border-[#E5E5E5]
-                     text-dark
-                     hover:text-white hover:bg-primary hover:border-primary
-                     mr-3
-                     sm:mr-4
-                     lg:mr-3
-                     xl:mr-4
-                     "
+       w-10
+       h-10
+       flex
+       items-center
+       justify-center
+       rounded-full
+       border border-[#E5E5E5]
+       text-dark
+       dark:text-white
+       hover:text-white
+       hover:bg-primary
+       hover:border-primary
+       mr-3
+       sm:mr-4
+       lg:mr-3
+       xl:mr-4
+    "
     >
         <svg
             width="16"
@@ -58,22 +66,26 @@
         </svg>
     </a>
     <a
-        href="javascript:void(0)"
+        href="https://youtube.com/mb"
+        target="_blank"
         class="
-                     w-8
-                     h-8
-                     flex
-                     items-center
-                     justify-center
-                     rounded-full
-                     border border-[#E5E5E5]
-                     text-dark
-                     hover:text-white hover:bg-primary hover:border-primary
-                     mr-3
-                     sm:mr-4
-                     lg:mr-3
-                     xl:mr-4
-                     "
+       w-10
+       h-10
+       flex
+       items-center
+       justify-center
+       rounded-full
+       border border-[#E5E5E5]
+       text-dark
+       dark:text-white
+       hover:text-white
+       hover:bg-primary
+       hover:border-primary
+       mr-3
+       sm:mr-4
+       lg:mr-3
+       xl:mr-4
+    "
     >
         <svg
             width="16"
@@ -89,20 +101,21 @@
     <a
         href="javascript:void(0)"
         class="
-                     w-8
-                     h-8
-                     flex
-                     items-center
-                     justify-center
-                     rounded-full
-                     border border-[#E5E5E5]
-                     text-dark
-                     hover:text-white hover:bg-primary hover:border-primary
-                     mr-3
-                     sm:mr-4
-                     lg:mr-3
-                     xl:mr-4
-                     "
+       w-10
+       h-10
+       flex
+       items-center
+       justify-center
+       rounded-full
+       border border-[#E5E5E5]
+       text-dark
+        dark:text-white
+       hover:text-white hover:bg-primary hover:border-primary
+       mr-3
+       sm:mr-4
+       lg:mr-3
+       xl:mr-4
+    "
     >
         <svg
             width="14"
@@ -116,65 +129,3 @@
         </svg>
     </a>
 </div>
-<p class="text-base text-body-color">&copy; 2025 TailGrids</p>
-</div>
-</div>
-</div>
-</div>
-<div>
-      <span class="absolute left-0 bottom-0 z-[-1]">
-         <svg
-             width="217"
-             height="229"
-             viewBox="0 0 217 229"
-             fill="none"
-             xmlns="http://www.w3.org/2000/svg"
-         >
-            <path
-                d="M-64 140.5C-64 62.904 -1.096 1.90666e-05 76.5 1.22829e-05C154.096 5.49924e-06 217 62.904 217 140.5C217 218.096 154.096 281 76.5 281C-1.09598 281 -64 218.096 -64 140.5Z"
-                fill="url(#paint0_linear_1179_5)"
-            />
-            <defs>
-               <linearGradient
-                   id="paint0_linear_1179_5"
-                   x1="76.5"
-                   y1="281"
-                   x2="76.5"
-                   y2="1.22829e-05"
-                   gradientUnits="userSpaceOnUse"
-               >
-                  <stop stop-color="#3056D3" stop-opacity="0.08" />
-                  <stop offset="1" stop-color="#C4C4C4" stop-opacity="0" />
-               </linearGradient>
-            </defs>
-         </svg>
-      </span>
-    <span class="absolute top-10 right-10 z-[-1]">
-         <svg
-             width="75"
-             height="75"
-             viewBox="0 0 75 75"
-             fill="none"
-             xmlns="http://www.w3.org/2000/svg"
-         >
-            <path
-                d="M37.5 -1.63918e-06C58.2107 -2.54447e-06 75 16.7893 75 37.5C75 58.2107 58.2107 75 37.5 75C16.7893 75 -7.33885e-07 58.2107 -1.63918e-06 37.5C-2.54447e-06 16.7893 16.7893 -7.33885e-07 37.5 -1.63918e-06Z"
-                fill="url(#paint0_linear_1179_4)"
-            />
-            <defs>
-               <linearGradient
-                   id="paint0_linear_1179_4"
-                   x1="-1.63917e-06"
-                   y1="37.5"
-                   x2="75"
-                   y2="37.5"
-                   gradientUnits="userSpaceOnUse"
-               >
-                  <stop stop-color="#13C296" stop-opacity="0.31" />
-                  <stop offset="1" stop-color="#C4C4C4" stop-opacity="0" />
-               </linearGradient>
-            </defs>
-         </svg>
-      </span>
-</div>
-
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,7 +7,6 @@
             ['label' => 'Contact', 'href' => '#contact' ],
         ]
     @endphp
-
     <!DOCTYPE html>
     <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
@@ -21,13 +20,11 @@
     </head>
 
     <body class="antialiased text-gray-800 dark:text-gray-200">
-
-
+    <div class="min-h-screen bg-gray-100 dark:bg-gray-900 pt-24">
         <x-layout.navbar :items="$navigationItems"></x-layout.navbar>
         {{ $slot }}
-    
-
+        <x-layout.footer></x-layout.footer>
+    </div>
     <script src="{{ asset('js/app.js') }}"></script>
-    <x-layout.footer></x-layout.footer>
     </body>
     </html>
